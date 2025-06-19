601c34d81239178f234232b82581c3dac06d0255
unknown <xandyei03@gmail.com>
2025-06-19 14:22:00 -0300

    index code

---
+++ QUESTÃO-1/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head><title>Livros</title></head>
+<body>
+<h1>Lista de Livros</h1>
+
+<h2>Adicionar Livro</h2>
+<form >
+    <input type="text" name="titulo" placeholder="Título"><br>
+    <input type="text" name="autor" placeholder="Autor"><br>
+    <input type="number" name="ano" placeholder="Ano"><br>
+    <input type="submit" value="Adicionar">
+</form>
+</body>
+</html>
