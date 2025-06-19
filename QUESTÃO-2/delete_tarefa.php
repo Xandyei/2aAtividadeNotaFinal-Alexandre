b2b6e548b5e44f5d8badb111d67f2295e3ab36cc
unknown <xandyei03@gmail.com>
2025-06-19 15:38:22 -0300

    arrumado o codigo de deletar tarefa

--- QUESTÃO-2/delete_tarefa.php
+++ QUESTÃO-2/delete_tarefa.php
@@ -1,9 +1,10 @@
 <?php
 if (isset($_GET["id"])) {
     include "database.php";
-    $comando = $banco->prepare("UPDATE tarefas SET concluida = 1 WHERE id = ?");
+
+    $comando = $banco->prepare("DELETE FROM tarefas WHERE id = ?");
     $comando->execute([$_GET["id"]]);
+
     header("Location: index.php");
     exit();
 }
-?>
