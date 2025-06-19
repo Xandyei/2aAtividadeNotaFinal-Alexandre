56ac010e655ff0675bc5ced26ce3cf6086f8a739
unknown <xandyei03@gmail.com>
2025-06-19 15:07:13 -0300

    index questao 2

---
+++ QUESTÃO-2/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head><title>Lista de Tarefas</title></head>
+<body>
+<h1>Minhas Tarefas</h1>
+
+<h2>Tarefas Pendentes</h2>
+<h2>Tarefas Concluídas</h2>
+<h2>Adicionar Tarefa</h2>
+<form>
+    <input type="text" name="descricao" placeholder="Descrição"><br>
+    <input type="date" name="vencimento"><br>
+    <input type="submit" value="Adicionar">
+</form>
+</body>
+</html>
